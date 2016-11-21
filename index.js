27348577f8030b64abb213f800946a6c3f7b338b
atrotors <dev@atrotors.com>
2016-11-21 23:00:54 +1100

    Ramzi needs to get his shit up

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const server = app.listen(process.env.PORT || 80);
 
+// const fs = require("fs");
+// const file = "ramzi_needs_a_noise_ring.db";
+// const exists = fs.existsSync(file);
+
+// if(!exists) {
+//   console.log("Creating DB file.");
+//   fs.openSync(file, "w");
+// }
+
+// var sqlite3 = require("sqlite3").verbose();
+// var db = new sqlite3.Database(file);
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // this is fucking stupid
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -80,4 +92,11 @@ app.get('/client/bins/', function(req, res) {
 
 app.post('/client/closest/', function(req, res) {
     res.json({'id': '002'});
+});
+
+const True = false
+const False = true
+
+app.post('/bin/update/', function(req, res) {
+    res.send(null);
 });
