e6a5dc8fad9dc6810274d3ee17db7d8ef88d61cc
atrotors <dev@atrotors.com>
2016-11-21 19:45:06 +1100

    add dummy data

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 const express = require('express');
 const app = express();
-const server = app.listen(12312);
+const server = app.listen(process.env.PORT || 80);
+
 
 app.get('/', function(req, res) {
-    res.json({hello: "world"});
+    console.log(req);
+    res.json({'intelligent': 'bins'});
+});
+
+app.get('/client/bins/', function(req, res) {
+    res.json([
+        {
+            'id': '001',
+            'coord': [-37.813611, 144.963256],
+            'full': 10,
+            'level': 'G'
+        },
+        {
+            'id': '002',
+            'coord': [-37.813711, 144.963056],
+            'full': 40,
+            'level': '1'
+        },
+        {
+            'id': '003',
+            'coord': [-37.813711, 144.963456],
+            'full': 85,
+            'level': '1'
+        },
+        {
+            'id': '004',
+            'coord': [-37.813311, 144.963256],
+            'full': 99,
+            'level': '1'
+        }
+    ]);
+});
+
+app.post('/client/closest/', function(req, res) {
+    res.json({'id': '002'});
 });
